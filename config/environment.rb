b4b3b3a9d574aa793b0dbe19546d8646971d6fdc
Andrew <andrew.cai@employmentinnovations.com>
2017-11-15 22:08:10 +1100

    added mailer receiving order/shipping and tests for them

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Depot::Application.configure do
+  config.action_mailer.delivery_method = :test
+end
