cd26e5acb589e41eadbda59277b182712aece8cf
Andrew <andrew.cai@employmentinnovations.com>
2017-11-20 21:07:16 +1100

    added validation on product names

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,8 +4,9 @@ class Product < ApplicationRecord
   before_destroy :ensure_not_referenced_by_any_line_item
 
   validates :title, :description, :image_url, presence: true
-  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   validates :title, uniqueness: true
+  validates :title, length: { minimum: 10, message: 'must be at least 10 characters long'}
   validates :image_url, allow_blank: true, format: {
       with:    %r{\.(gif|jpg|png)\Z}i,
       message: 'must be a URL for GIF, JPG or PNG image.' 
