2d39da10cc36da7cbfb0978668d8f7679a9b1c5c
Andrew <andrew.cai@employmentinnovations.com>
2017-11-20 18:50:48 +1100

    updated user view, added user tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = User.order(:name)
   end
 
   # GET /users/1
@@ -28,8 +28,8 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
+        format.html { redirect_to users_url,
+                      notice: "User {@user.name} was successfully created." }
       else
         format.html { render :new }
         format.json { render json: @user.errors, status: :unprocessable_entity }
@@ -42,8 +42,8 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
+        format.html { redirect_to users_url,
+                      notice: "User {@user.name} was successfully created." }
       else
         format.html { render :edit }
         format.json { render json: @user.errors, status: :unprocessable_entity }
